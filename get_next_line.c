ed0baf4411b8ee3a16cdb0d8633b8d9eace1c095
HanFeg-42 <hananefegrachi2002@gmail.com>
2024-11-23 11:56:17 +0100

    trying to figure out how read() works

---
+++ get_next_line.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfegrach <hfegrach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 11:39:28 by hfegrach          #+#    #+#             */
+/*   Updated: 2024/11/23 11:51:35 by hfegrach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char *get_next_line(int fd);
+
+int main()
+{
+    char *str = malloc(10);
+    int fd = open("file.txt", O_CREAT | O_RDONLY, 0640);
+    if (fd == -1)
+        return 2;
+    int i = 5;
+    while(i)
+    {
+        if (read(fd, str, 6) != -1 != -1)
+        {
+            printf("%s\n", str);
+        }
+        i--;
+    }     
+    return 0;
+}
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfegrach <hfegrach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 11:39:37 by hfegrach          #+#    #+#             */
+/*   Updated: 2024/11/23 11:42:43 by hfegrach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+char *get_next_line(int fd);
+
+#endif
