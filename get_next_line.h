e3ebe068da170b6296e606d3da05f3f120dcff2d
HanFeg-42 <hananefegrachi2002@gmail.com>
2024-11-25 09:49:29 +0100

    commit test

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,20 @@
 /*   By: hfegrach <hfegrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 11:39:37 by hfegrach          #+#    #+#             */
-/*   Updated: 2024/11/24 20:50:48 by hfegrach         ###   ########.fr       */
+/*   Updated: 2024/11/25 09:48:26 by hfegrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdint.h>
+# include <fcntl.h>
 
 //# define BUFFER_SIZE
 
