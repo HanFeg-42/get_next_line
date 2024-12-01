bd75151e777afa434c458fb8aff3a3fc0412c601
HanFeg-42 <hananefegrachi2002@gmail.com>
2024-12-01 10:23:46 +0100

    I resized it all good

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,14 @@
 /*   By: hfegrach <hfegrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 11:39:28 by hfegrach          #+#    #+#             */
-/*   Updated: 2024/12/01 09:48:39 by hfegrach         ###   ########.fr       */
+/*   Updated: 2024/12/01 10:22:51 by hfegrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/*========================YARBI TKOUN THE LAST TRY=======================*/
-// ------------------it works --------------------------do not touch it----
-
+/*========================bghit ghi ntester wahed l3iba=======================*/
+//---------------------------it worked wakha sghrtha--------------------------------//
 void	fill_in_save(int fd, ssize_t *rd, char **save, char **buff)
 {
 	while (1)
@@ -27,39 +26,27 @@ void	fill_in_save(int fd, ssize_t *rd, char **save, char **buff)
 			*save = ft_strdup(*buff);
 		else
 			*save = ft_strjoin(*save, *buff);
-		if (!(*save))
-			break ;
 		if (ft_strchr(*save, '\n'))
 			break ;
 	}
 }
 
-char	*test(char **save)
-{
-	char	*tmp;
-
-	tmp = ft_strdup(*save);
-	if (!tmp)
-		return (*save = NULL, NULL);
-	return (free(*save), *save = NULL, tmp);
-}
-
-char	*read_line(char **save)
+char	*read_line(char **save, ssize_t rd)
 {
 	char	*line;
 	char	*new;
 
-	line = ft_substr(*save, 0, ft_strchr(*save, '\n') - *save + 1);
-	if (!line)
-		return (free(line), line = NULL, NULL);
-	new = ft_strdup(ft_strchr(*save, '\n') + 1);
-	if (!new)
-		return (free(*save), free(line), *save = NULL, NULL);
-	free(*save);
-	*save = ft_strdup(new);
-	if (!(*save))
-		return (free(new), free(line), *save = NULL, NULL);
-	return (free(new), line);
+	if (*save != NULL && ft_strchr(*save, '\n'))
+	{
+		line = ft_substr(*save, 0, ft_strchr(*save, '\n') - *save + 1);
+		new = ft_strdup(ft_strchr(*save, '\n') + 1);
+		free(*save);
+		*save = ft_strdup(new);
+		return (free(new), line);
+	}
+	else if (!rd && *save != NULL)
+		return (new = ft_strdup(*save), free(*save), *save = NULL, new);
+	return (NULL);
 }
 
 char	*get_next_line(int fd)
@@ -75,9 +62,75 @@ char	*get_next_line(int fd)
 		return (NULL);
 	fill_in_save(fd, &rd, &save, &buff);
 	free(buff);
-	if (save != NULL && ft_strchr(save, '\n'))
-		return (read_line(&save));
-	else if (!rd && save != NULL)
-		return (test(&save));
-	return (NULL);
+	return (read_line(&save, rd));
 }
+
+/*========================YARBI TKOUN THE LAST TRY=======================*/
+// ------------------it works --------------------------do not touch it----
+
+// void	fill_in_save(int fd, ssize_t *rd, char **save, char **buff)
+// {
+// 	while (1)
+// 	{
+// 		*rd = read(fd, *buff, BUFFER_SIZE);
+// 		if (*rd == 0 || *rd == -1)
+// 			break ;
+// 		(*buff)[*rd] = '\0';
+// 		if (!(*save))
+// 			*save = ft_strdup(*buff);
+// 		else
+// 			*save = ft_strjoin(*save, *buff);
+// 		if (!(*save))
+// 			break ;
+// 		if (ft_strchr(*save, '\n'))
+// 			break ;
+// 	}
+// }
+
+// char	*test(char **save)
+// {
+// 	char	*tmp;
+
+// 	tmp = ft_strdup(*save);
+// 	if (!tmp)
+// 		return (*save = NULL, NULL);
+// 	return (free(*save), *save = NULL, tmp);
+// }
+
+// char	*read_line(char **save)
+// {
+// 	char	*line;
+// 	char	*new;
+
+// 	line = ft_substr(*save, 0, ft_strchr(*save, '\n') - *save + 1);
+// 	if (!line)
+// 		return (free(line), line = NULL, NULL);
+// 	new = ft_strdup(ft_strchr(*save, '\n') + 1);
+// 	if (!new)
+// 		return (free(*save), free(line), *save = NULL, NULL);
+// 	free(*save);
+// 	*save = ft_strdup(new);
+// 	if (!(*save))
+// 		return (free(new), free(line), *save = NULL, NULL);
+// 	return (free(new), line);
+// }
+
+// char	*get_next_line(int fd)
+// {
+// 	static char	*save;
+// 	char		*buff;
+// 	ssize_t		rd;
+
+// 	if (fd < 0 || BUFFER_SIZE <= 0)
+// 		return (NULL);
+// 	buff = malloc((BUFFER_SIZE * sizeof(char)) + 1);
+// 	if (!buff)
+// 		return (NULL);
+// 	fill_in_save(fd, &rd, &save, &buff);
+// 	free(buff);
+// 	if (save != NULL && ft_strchr(save, '\n'))
+// 		return (read_line(&save));
+// 	else if (!rd && save != NULL)
+// 		return (test(&save));
+// 	return (NULL);
+// }
