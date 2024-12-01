a0ec1bb0c318be5b63bdba42fafb52647849d40e
HanFeg-42 <hananefegrachi2002@gmail.com>
2024-12-01 17:58:16 +0100

    TIMEOUT problem

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main()
 {
     //int fd = open("test_empty", O_CREAT | O_RDONLY | O_TRUNC, 0640);
     // int fd=open("/dev/null",O_RDONLY);
-    int fd = open("nos.txt", O_CREAT | O_RDWR, 0640);
+    int fd = open("file.txt", O_CREAT | O_RDWR, 0640);
     // write(fd, "nouss is\nvery\nbeautiful\nwa chokran", 34);
     // close(fd);
     // fd = open("nos.txt", O_CREAT | O_RDWR, 0640);
@@ -24,10 +24,10 @@ int main()
         printf("%s", s);
         free(s);
         s = get_next_line(fd);
-        printf("i have ---%s----", s);
+    
     }
-    // s = get_next_line(fd);
-    printf("--%s--", s);
+    // s = get_next_line(fd);    printf("i have ---%s----", s);
+    // printf("--%s--", s);
     //system("leaks a.out");
     return (0);
 }
